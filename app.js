9cb6162129e2ac52d3e17e33227db7cd97b97f0d
panda1 <blueblue0011@gmail.com>
2022-07-19 15:57:53 -0700

    stuck at 6.1

---
+++ app.js
@@ -0,0 +1,18 @@
+let h1_event = document.querySelector(`#hover_me`);
+
+let header_event = document.querySelectorAll(`header`);
+h1_event.addEventListener(`mouseover`, add_h3);
+
+function add_h3(details) {
+    // details[`target`].insertAdjacentHTML(`afterend`, `<h3 id = "to_remove"> secret hover!</h3>`)
+    h1_event.insertAdjacentHTML(`afterend`, `<h3>Secret hover!</h3>`);
+}
+
+function remove_h3() {
+    let h3_event = document.querySelectorAll(`header h3`);
+    h3_event[`outerHTML`] = ``;
+    // h1_event.insertAdjacentHTML(`beforebegin`, `<h1>hover me</h1>`);
+}
+
+h1_event.addEventListener(`mouseleave`, remove_h3);
+
